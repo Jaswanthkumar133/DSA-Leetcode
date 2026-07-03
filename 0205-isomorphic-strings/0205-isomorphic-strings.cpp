470afa6f697dc8025742ace9a515db54b9a88690
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-03 12:00:13 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,19 +4,19 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int>stoT;
-        unordered_map<char,int>Ttos;
+        unordered_map<char,char>stoT;
+        unordered_map<char,char>ttoS;
         for(int i=0;i<s.size();i++){
             if(stoT[s[i]]==0){
                 stoT[s[i]]=t[i];
-            }
-            if(stoT[s[i]]!=0 && stoT[s[i]]!=t[i]){
+            };
+            if(stoT[s[i]]!=t[i]){
                 return false;
             }
-            if(Ttos[t[i]]==0){
-                Ttos[t[i]]=s[i];
+            if(ttoS[t[i]]==0){
+                ttoS[t[i]]=s[i];
             }
-            if(Ttos[t[i]]!=0 && Ttos[t[i]]!=s[i]){
+            if(ttoS[t[i]]!=s[i]){
                 return false;
             }
         }
