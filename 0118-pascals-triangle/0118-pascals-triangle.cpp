db23e8bc6caac3d45f75d878bbce0cdd755c10d8
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-07-17 04:48:50 +0530

    Time: 0 ms (100%), Space: 9.8 MB (9.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fact(int n,int r){
+    int fact(int n, int r){
         int res=1;
         for(int i=0;i<r;i++){
             res*=(n-i);
@@ -9,14 +9,14 @@ public:
         return res;
     }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>temp;
+        vector<vector<int>>res;
         for(int i=0;i<numRows;i++){
-            vector<int>ans;
+            vector<int>temp;
             for(int j=0;j<=i;j++){
-                ans.push_back(fact(i,j));
+                temp.push_back(fact(i,j));
             }
-            temp.push_back(ans);
+            res.push_back(temp);
         }
-        return temp;
+        return res;
     }
 };
