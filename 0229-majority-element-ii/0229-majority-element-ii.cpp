9eb4c1fa5080f464dbbdd2b4321ded98ea32784b
Jaswanth__J <135996823+Jaswanthkumar133@users.noreply.github.com>
2026-09-19 00:02:01 +0530

    Time: 0 ms (100%), Space: 23.7 MB (73.27%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int c1=0;
-        int c2=0;
+        int count1=0;
+        int count2=0;
         int ele1=-1;
         int ele2=-1;
-        for(int i:nums){
-            if(i==ele1){
-                c1++;
-            }else if(i==ele2){
-                c2++;
-            }else if(c1==0){
-                ele1=i;
-                c1=1;
-            }else if(c2==0){
-                ele2=i;
-                c2=1;
-            }else{
-                c1--;
-                c2--;
-            }
-        }
-        c1=0;
-        c2=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==ele1){
-                c1++;
+                count1++;
             }else if(nums[i]==ele2){
-                c2++;
+                count2++;
+            }else if(count1==0){
+                ele1=nums[i];
+                count1=1;
+            }else if(count2==0){
+                ele2=nums[i];
+                count2=1;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        vector<int>res;
-        if(c1>=nums.size()/3+1){
-            res.push_back(ele1);
+        count1=0;
+        count2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele1){
+                count1++;
+            }else if(nums[i]==ele2){
+                count2++;
+            }
         }
-        if(c2>=nums.size()/3+1){
-            res.push_back(ele2);
+        vector<int>temp;
+        if(count1>=nums.size()/3+1){
+            temp.push_back(ele1);
         }
-        return res;
+        if(count2>=nums.size()/3+1){
+            temp.push_back(ele2);
+        }
+        return temp;
     }
 };
